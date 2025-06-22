22a2e17079415e864818ab2b1970bf43bfa725c7
piyush kheria <piyushkheria23@gmail.com>
2025-06-22 16:56:52 +0530

    "environ added"

--- app.py
+++ app.py
@@ -915,4 +915,5 @@ def generate_student_report_pdf(stats):
         return jsonify({"error": "Failed to generate PDF"}), 500
  
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)    
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port)  
