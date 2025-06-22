7bb4fac8add6af3a6852e1c0ecb113a63d89fd9a
piyush kheria <piyushkheria23@gmail.com>
2025-06-22 16:54:23 +0530

    "changed host"

--- app.py
+++ app.py
@@ -915,4 +915,4 @@ def generate_student_report_pdf(stats):
         return jsonify({"error": "Failed to generate PDF"}), 500
  
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)    
+    app.run(host='0.0.0.0', port=5000, debug=True)    
